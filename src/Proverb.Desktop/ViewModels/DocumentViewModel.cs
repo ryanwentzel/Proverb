ee82d09a4e1e643def498dac3387948f625e0dde
Ryan Wentzel <ryan.wentzel@gmail.com>
2012-12-20 15:55:56 -0800

    Added DocumentViewModel class

--- src/Proverb.Desktop/ViewModels/DocumentViewModel.cs
+++ src/Proverb.Desktop/ViewModels/DocumentViewModel.cs
@@ -4,5 +4,25 @@ namespace Proverb.ViewModels
 {
     public sealed class DocumentViewModel : Screen
     {
+        private const int DefaultFontSize = 16;
+
+        private int _fontSize;
+        public int FontSize
+        {
+            get
+            {
+                return _fontSize;
+            }
+            set
+            {
+                _fontSize = value;
+                NotifyOfPropertyChange(() => FontSize);
+            }
+        }
+
+        public DocumentViewModel()
+        {
+            FontSize = DefaultFontSize;
+        }
     }
 }
